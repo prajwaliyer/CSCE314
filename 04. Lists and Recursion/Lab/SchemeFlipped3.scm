b6c5a0cbf55e8cd2cef951702f1945ff05dadd59
Adil Rasiyani <=>
2020-03-23 17:49:58 -0500

    Add comments

--- 04. Lists and Recursion/Lab/SchemeFlipped3.scm
+++ 04. Lists and Recursion/Lab/SchemeFlipped3.scm
@@ -2,14 +2,15 @@
 ; Lab 4, Program 3
 ; Merge sorted lists
 
+; Have to check 4 cases. (l1 & l2 are empty, l1 empty, l2 empty, neither)
 (define (merge l1 l2)
-    (if (and (null? l1) (null? l2))
+    (if (and (null? l1) (null? l2))                     ; Both lists are empty
         '()
-        (if (null? l1)
+        (if (null? l1)                                  ; Only the first list is empty
             (cons (car l2) (merge l1 (cdr l2)))
-            (if (null? l2)
+            (if (null? l2)                              ; Only the second list is empty
                 (cons (car l1) (merge (cdr l1) l2))
-                (if (< (car l2) (car l1))
+                (if (< (car l2) (car l1))               ; Neither list is empty
                     (cons (car l2) (merge l1 (cdr l2)))
                     (cons (car l1) (merge (cdr l1) l2))
                 )
